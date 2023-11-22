92a5d2cad3440b8526b305a2b071d3fb11335a9e
abazakeev <bazakeev04@gmail.com>
2023-11-22 14:21:23 +0600

    Added FileManager
    Added the ability to determine the presence of a flash drive, content filtering, the ability to navigate through directories and subdirectories, a method for obtaining the selected file in Base64 format.

---
+++ FilePrintHub/WebApi/Controllers/FilesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Services;
+
+namespace WebApi.Controllers;
+
+[ApiController, Route("api/[controller]")]
+public class FilesController : ControllerBase
+{
+    private readonly FileManager _fileManager;
+
+    public FilesController()
+    {
+        _fileManager = new FileManager();
+    }
+
+    [HttpGet("flash-drive")]
+    public IActionResult CheckFlashDrive()
+    {
+        bool hasFlashDrive = _fileManager.CheckFlashDrive();
+        return Ok(hasFlashDrive);
+    }
+
+    [HttpGet("files")]
+    public IActionResult GetFilesInDirectory([FromQuery] string path)
+    {
+        string[] files = _fileManager.GetFilesInDirectory(path);
+        return Ok(files);
+    }
+
+    [HttpGet("filtered-files")]
+    public IActionResult GetFilteredFiles([FromQuery] string path)
+    {
+        string[] filteredFiles = _fileManager.GetFilteredFiles(path);
+        return Ok(filteredFiles);
+    }
+
+    [HttpGet("file-base64")]
+    public IActionResult GetFileAsBase64([FromQuery] string filePath)
+    {
+        string fileBase64 = _fileManager.GetFileAsBase64(filePath);
+        return Ok(fileBase64);
+    }
+}
---
+++ FilePrintHub/WebApi/Services/FileManager.cs
@@ -0,0 +1,33 @@
+﻿namespace WebApi.Services;
+
+public class FileManager
+{
+    public bool CheckFlashDrive()
+    {
+        DriveInfo[] drives = DriveInfo.GetDrives();
+        return drives.Any(drive => drive.DriveType == DriveType.Removable);
+    }
+
+    public string[] GetFilesInDirectory(string path)
+    {
+        return Directory.GetFiles(path);
+    }
+
+    public string[] GetFilteredFiles(string path)
+    {
+        string[] allowedExtensions = new string[] { ".doc", ".docx", ".xls", ".xlsx", ".pdf", ".jpg", ".png" };
+        return Directory.GetFiles(path)
+                        .Where(file => allowedExtensions.Contains(Path.GetExtension(file)))
+                        .ToArray();
+    }
+
+    public string GetFileAsBase64(string filePath)
+    {
+        byte[] fileBytes = File.ReadAllBytes(filePath);
+        string fileName = Path.GetFileName(filePath);
+        string fileExtension = Path.GetExtension(filePath);
+        long fileSize = new FileInfo(filePath).Length;
+
+        return $"Name: {fileName}, Extension: {fileExtension}, Size: {fileSize}, Base64: {Convert.ToBase64String(fileBytes)}";
+    }
+}
